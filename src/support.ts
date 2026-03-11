2b5b908e05d15f59d6a7138d184ff3a6714666c8
Sigaev Nikita <sigaev.n.a@yandex.ru>
2026-03-11 16:23:05 +0300

    Change linux chrome bug condition

--- src/support.ts
+++ src/support.ts
@@ -2,5 +2,5 @@ export function isEyeDropperSupported(): boolean {
   const userAgent = navigator.userAgent.toLowerCase();
   const linuxChromeBug = userAgent.includes('linux') && userAgent.includes('chrome')
   const nativeEyeDropperExist = 'EyeDropper' in window;
-  return linuxChromeBug || nativeEyeDropperExist;
+  return !linuxChromeBug && nativeEyeDropperExist;
 }
